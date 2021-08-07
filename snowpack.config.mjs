a6ad677f7a3eeb53c3128766957d3b8f899b98a0
lenin jose mendoza espina <mendoza124302@gmail.com>
2021-08-06 23:49:28 -0500

    modificando rutas

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,27 +1,29 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
-  mount: {
-    public: { url: '/', static: true },
-    src: { url: '/dist' },
-  },
-  plugins: [
-    /* ... */
-  ],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
-  optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
-  },
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
+    mount: {
+        public: { url: '/', static: true },
+        images: { url: '/images', static: true },
+        css: { url: '/css', static: true },
+        src: { url: '/dist' },
+    },
+    plugins: [
+        /* ... */
+    ],
+    routes: [
+        /* Enable an SPA Fallback in development: */
+        // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    ],
+    optimize: {
+        /* Example: Bundle your final build: */
+        // "bundle": true,
+    },
+    packageOptions: {
+        /* ... */
+    },
+    devOptions: {
+        /* ... */
+    },
+    buildOptions: {
+        /* ... */
+    },
 };
